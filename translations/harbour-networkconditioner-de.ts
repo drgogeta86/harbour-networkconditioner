3b6c6c341c04945b73a5fd205c54fe9d2760372a
dr_gogeta86 <dr_gogeta86@messinalug.org>
2014-07-02 22:13:55 +0200

    Initial Commit .... just ui things

---
+++ translations/harbour-networkconditioner-de.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>NetworkConditioner about strings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation>Mein Cover</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Show Page 2</source>
+        <translation type="obsolete">Zur Seite 2</translation>
+    </message>
+    <message>
+        <source>UI Template</source>
+        <translation type="obsolete">UI-Vorlage</translation>
+    </message>
+    <message>
+        <source>Hello Sailors</source>
+        <translation type="obsolete">Hallo Matrosen</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SpeedTest Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Network Conditioner</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Put Graph here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Active Profile:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Nested Page</source>
+        <translation>Unterseite</translation>
+    </message>
+    <message>
+        <source>Item</source>
+        <translation>Element</translation>
+    </message>
+</context>
+</TS>
